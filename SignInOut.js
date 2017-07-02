84972c7e13fb040fe9338a833d80186a63b58810
Quinton Swanepoel <u15245510@tuks.co.za>
2017-07-02 12:44:44 +0200

    Update SignInOut.js

--- SignInOut.js
+++ SignInOut.js
@@ -5,47 +5,6 @@ var bodyParser = require('body-parser');
 var app = express();
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
-app.post('/', urlencodedParser, (req, res) =>{
-        res.status(200).end();
-        var reqBody = req.body;
-        var responseURL = reqBody.response_url;
-        if(reqBody.token != 'CVuvCq700N50MgszRrYNn5x7'){
-                res.status(403).end("Access forbidden");
-        }else{
-                var message = {
-                        "text": "Sign In/Out",
-    "response_type": "ephemeral",
-    "replace_original" : true,
-    "attachments": [
-        {
-            "text": "Are you signing in or out?",
-            "fallback": "You are unable",
-            "callback_id": "SignInOut_selection",
-            "color": "#3AA3E3",
-            "name": "SignInOut_list",
-            "attachment_type": "default",
-            "actions": [
-                {
-                    "name": "SignIn",
-                    "type": "button",
-                    "text": "Sign in",
-                    "value": "Sign in"
-                },
-                {
-                    "name": "SignOut",
-                    "type": "button",
-                    "text": "Sign out",
-                    "value": "Sign out"
-                }
-            ]
-        }
-    ]
-}
-
-sendMessageToSlackResponseURL(responseURL, message);
-}
-});
-
 function sendMessageToSlackResponseURL(responseURL, JSONmessage){
     var postOptions = {
         url: responseURL,
@@ -62,6 +21,47 @@ request(postOptions, (error, response, body) => {
     })
 }
 
+app.post('/', urlencodedParser, (req, res) =>{
+        res.status(200).end();
+        var reqBody = req.body;
+        var responseURL = reqBody.response_url;
+        if(reqBody.token != 'CVuvCq700N50MgszRrYNn5x7')
+        {
+                res.status(403).end("Access forbidden");
+        }else{
+                
+                var message = {
+                    "text": "Sign In/Out",
+                    "response_type": "ephemeral",
+                    "replace_original" : true,
+                    "attachments": [
+                        {
+                            "text": "Are you signing in or out?",
+                            "fallback": "You are unable",
+                            "callback_id": "SignInOut_selection",
+                            "color": "#3AA3E3",
+                            "name": "SignInOut_list",
+                            "attachment_type": "default",
+                            "actions": [
+                                {
+                                    "name": "SignIn",
+                                    "type": "button",
+                                    "text": "Sign in",
+                                    "value": "Sign in"
+                                },
+                                {
+                                    "name": "SignOut",
+                                    "type": "button",
+                                    "text": "Sign out",
+                                    "value": "Sign out"
+                                }
+                            ]
+                        }
+                    ]
+                }
+                sendMessageToSlackResponseURL(responseURL, message);
+        }
+});
 
 var port = 9000;
 app.listen(port);
