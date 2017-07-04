d52c1c8b3f87a617735db6e8d2040e04d70766df
Quinton Swanepoel <u15245510@tuks.co.za>
2017-07-04 09:31:08 +0200

    Update InteractiveButtons.js

--- InteractiveButtons.js
+++ InteractiveButtons.js
@@ -403,7 +403,7 @@ app.post('/', urlencodedParser, (req, res) =>{
                  console.log("MESSAGE HoursEnd");
                 var temp = "[";
                  for(var i=0;i<=actionJSONPayload.count;i++){
-                     temp += '{"text": "' + (i+1) + '," "value": ' + (i+1) + '},';
+                     temp += '{"text": "' + (i+1) + '"," "value": "' + (i+1) + '"},';
                  }
                      temp = temp.substring(0,temp.length-1);
                          temp += "]";
