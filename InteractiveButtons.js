2dd76eb21c0f08368fe1c31580682067063ff3ff
Quinton Swanepoel <u15245510@tuks.co.za>
2017-07-04 09:48:45 +0200

    Update InteractiveButtons.js

--- InteractiveButtons.js
+++ InteractiveButtons.js
@@ -410,7 +410,7 @@ app.post('/', urlencodedParser, (req, res) =>{
              var message = '{ "text": "Work Records","response_type": "ephemeral","replace_original" : true,"attachments": [{"text": "Which work record would you like to resubmit?",';
              message += '"fallback": "Not Available","color": "#3AA3E3","attachment_type": "default","callback_id": "record_selection", "actions": [{';                 
              message += '"name": "project_list","text": "Which work record would you like to resubmit?","type": "select","options":'; 
-             message += temp;
+             message += temp.toStrin();
             message += '}]}]}';
             console.log(message + "\n\n");
               /* var message = {
@@ -439,7 +439,7 @@ app.post('/', urlencodedParser, (req, res) =>{
      
                           
                                         console.log(JSON.parse(message));
-                 sendMessageToSlackResponseURL(actionJSONPayload.response_url, message); 
+                 sendMessageToSlackResponseURL(actionJSONPayload.response_url, JSON.parse(message)); 
         }
         else if(actionJSONPayload.callback_id == "TEST")
                 {
