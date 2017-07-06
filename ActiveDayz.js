2a74742b9d1bfe580555375a3c515faf8ecb421b
Quinton Swanepoel <u15245510@tuks.co.za>
2017-07-06 22:14:41 +0200

    Update ActiveDayz.js

--- ActiveDayz.js
+++ ActiveDayz.js
@@ -1,1 +1,32 @@
+var http = require('http');
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var app = express();
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
+function sendMessageToSlackResponseURL(responseURL, JSONmessage){
+    var postOptions = {
+        url: responseURL,
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        json: JSONmessage
+    }
+request(postOptions, (error, response, body) => {
+        if (error){
+            // handle errors as you see fit
+        }
+    })
+}
+
+app.post('/', urlencodedParser, (req, res) =>{
+        res.status(200).end();
+        console.log(req);
+       
+});
+
+var port = 9000;
+app.listen(port);
+console.log('Listening on port',port);
