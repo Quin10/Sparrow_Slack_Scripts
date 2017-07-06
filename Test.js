e32f5adb5a731b845989c3b2531c16df2ef52fef
Quinton Swanepoel <u15245510@tuks.co.za>
2017-07-06 09:27:11 +0200

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,13 +2,15 @@ var Slack = require('node-slack-upload');
 var slack = new Slack("xoxp-148741775620-195455982320-203550966192-bcd0408fb6fc789587db6b30574b2a5c");
 
 
-
+app.post('/', urlencodedParser, (req, res) =>{
+        res.status(200).end();
+        var actionJSONPayload = JSON.parse(req.body.payload);
 slack.uploadFile({
-    file: fs.createReadStream(path.join(__dirname, '..', 'README.md')),
+    file: fs.createReadStream(path.join(actionJSONPayload.inlineImages, '..', 'README.md')),
     filetype: 'post',
     title: 'README',
     initialComment: 'my comment',
-    channels: 'XXXXX'
+    channels: 'quinton_testing'
 }, function(err, data) {
     if (err) {
         console.error(err);
@@ -16,4 +18,5 @@ slack.uploadFile({
     else {
         console.log('Uploaded file details: ', data);
     }
+});
 });
